fe9fce1f93eae99dd53570be1d836c58a0dd5817
Mohamed Ali Bouhoum <m.ali.bouhoum@gmail.com>
2020-12-19 02:19:33 +0100

    adjusting colors

--- colorkraken-config.js
+++ colorkraken-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     arguments: [
       { name: "primary", value: "#59C0D7" },
-      { name: "secondary", value: "#94292F" },
+      { name: "secondary", value: "#E0DC01" },
     ],
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header({headerData}) {
   return (
     <div>
       <div className=" w-3/12 mx-auto">
-        <h2 className="uppercase text-secondary-500 font-semibold text-2xl text-shadow-secondary">
+        <h2 className="uppercase text-secondary-500 font-black text-5xl text-shadow-secondary">
           {headerData}
         </h2>
         <div className="border-b-4 border-t-2 h-2 border-solid border-opacity-40 border-red-700 mt-2 mb-4"></div>
@@ -14,22 +14,22 @@ function Header({headerData}) {
         </h4>
         <div className="flex justify-start mt-2">
           <img
-            className="w-3 mr-2 opacity-70 transform scale-100 hover:scale-110 cursor-pointer hover:opacity-100 transition easy-in "
+            className="w-3 mr-2 transform scale-100 hover:scale-125 cursor-pointer transition easy-in active-hack-item-svg"
             src={ram}
             alt="CYBERDECK RAMS"
           />
           <img
-            className="w-3 mr-2 opacity-70 transform scale-100 hover:scale-110 cursor-pointer hover:opacity-100 transition easy-in active-hack-item-svg"
+            className="w-3 mr-2 transform scale-100 hover:scale-125 cursor-pointer transition easy-in active-hack-item-svg"
             src={ram}
             alt="CYBERDECK RAMS"
           />
           <img
-            className="w-3 mr-2 opacity-70 transform scale-100 hover:scale-110 cursor-pointer hover:opacity-100 transition easy-in active-hack-item-svg"
+            className="w-3 mr-2 transform scale-100 hover:scale-125 cursor-pointer transition easy-in active-hack-item-svg"
             src={ram}
             alt="CYBERDECK RAMS"
           />
           <img
-            className="w-3 mr-2 opacity-70 transform scale-100 hover:scale-110 cursor-pointer hover:opacity-100 transition easy-in active-hack-item-svg"
+            className="w-3 mr-2 transform scale-100 hover:scale-125 cursor-pointer transition easy-in active-hack-item-svg"
             src={ram}
             alt="CYBERDECK RAMS"
           />
